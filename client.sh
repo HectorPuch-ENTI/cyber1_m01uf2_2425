0e466d61f65db7d9ad3410c90c1e33d573641419
Hector <hector.puch@enti.cat>
2024-12-03 14:29:09 +0100

    Update

--- client.sh
+++ client.sh
@@ -1,1 +1,30 @@
 #!/bin/bash
+
+echo "LSTP Client(Lechuga Speaker Transfer Protocol)"
+
+echo "1. SEND HEADER"
+
+echo "LSTP_1" | nc localhost 7777
+
+echo "2.LISTEN OK_HEADER"
+
+DATA=`nc -l 7777`
+
+echo "6. CHECK OK_HEADER"
+
+if [ "$DATA" != "OK_HEADER" ]
+then
+	echo "ERROR 1: Header enviado incorrectamente."
+
+	exit 1
+fi
+
+#cat client/lechuga1.lechu | text2wave -o client/lechuga1.wav
+
+#yes | ffmpeg -i client/lechuga1.wav client/lechuga1.ogg
+
+echo "7. SEND FILE_NAME"
+
+echo "FILE_NAME lechuga.ogg"  | nc localhost 7777
+
+
