e1a885778fe5be98790f901980f22d4bc39dd8f8
Hector Puch <hector.puch@enti.cat>
2025-01-20 20:18:33 +0100

    Update

--- server.sh
+++ server.sh
@@ -1,37 +1,107 @@
 #!/bin/bash
 
-echo "LSTP server (Lechuga Speaker Transfer Protocol)"
+PORT=7777
+IP_CLIENT="localhost"
+
+echo "LSTP Server (Lechuga Speaker Transfer Protocol)"
 
 echo "0. LISTEN"
 
-DATA=`nc -l 7777`
+DATA=`nc -l $PORT`
 
 echo "3. CHECK HEADER"
 
-if [ "$DATA" != "LSTP_1" ]
-then
-	echo "ERROR 1: Header mal formado. $DATA"
+HEADER=`echo "$DATA" | cut -d " " -f 1`
 
-	echo "KO_HEADER" | nc localhost 7777
+if [ "$HEADER" != "LSTP_1" ]
+then
+	echo "ERROR 1: Header mal formado $DATA"
+
+	echo "KO_HEADER" | nc $IP_CLIENT $PORT
 
 	exit 1
 fi
 
+IP_CLIENT=`echo "$DATA" | cut -d " " -f 2`
+
 echo "4. SEND OK_HEADER"
 
-echo "OK_HEADER" | nc localhost 7777
+echo "OK_HEADER" | nc $IP_CLIENT $PORT
 
 echo "5. LISTEN FILE_NAME"
 
-FILES=`nc -l 7777 | cut -d' ' -f1`
+DATA=`nc -l $PORT`
 
 echo "9. CHECK FILE_NAME"
 
-if [ "$FILES" != "FILE_NAME" ]
-then
-	echo "ERROR 2: FILE_NAME mal formado. $FILES"
+PREFIX=`echo $DATA | cut -d " " -f 1`
 
-	echo "KO_FILE_NAME" | nc localhost 7777
+if [ "$PREFIX" != "FILE_NAME" ]
+then
+	echo "ERROR 2: FILE_NAME incorrecto"
+
+	echo "KO_FILE_NAME" | nc $IP_CLIENT $PORT
 
 	exit 2
 fi
+
+FILE_NAME=`echo $DATA | cut -d " " -f 2`
+
+echo "10. SEND OK_FILE_NAME"
+
+echo "OK_FILE_NAME" | nc $IP_CLIENT $PORT
+
+echo "11. LISTEN FILE DATA"
+
+nc -l $PORT > server/$FILE_NAME
+
+echo "14. SEND KO/OK_FILE_DATA"
+
+DATA=`cat server/$FILE_NAME | wc -c`
+
+if [ $DATA -eq 0 ]
+then
+	echo "ERROR 3: Datos mal formados (vacíos)"
+
+	echo "KO_FILE_DATA" | nc $IP_CLIENT $PORT
+
+	exit 3
+fi
+
+echo "OK_FILE_DATA" | nc $IP_CLIENT $PORT
+
+echo "15. LISTEN FILE_DATA_MD5"
+
+DATA=`nc -l $PORT`
+
+echo "18. CHECK FILE_DATA_MD5"
+
+PREFIX=`echo $DATA | cut -d " " -f 1`
+
+if [ $PREFIX != "FILE_DATA_MD5" ]
+then
+	echo "ERROR 4: FILE_DATA_MD5 incorrecto"
+
+	echo "KO_FILE_DATA_MD5" | nc $IP_CLIENT $PORT  
+
+	exit 4
+fi
+
+RECEIVED_MD5=`echo $DATA | cut -d " " -f 2`
+LOCAL_MD5=`cat server/$FILE_NAME | md5sum | cut -d " " -f 1`
+
+if [ $RECEIVED_MD5 != $LOCAL_MD5 ]
+then
+	echo "ERROR 5: HASH enviado y local distintos"
+
+	echo "KO_FILE_DATA_MD5" | nc $IP_CLIENT $PORT
+
+	exit 5
+fi
+
+echo "19. SEND OK_FILE_DATA_MD5"
+
+echo "OK_FILE_DATA_MD5" | nc $IP_CLIENT $PORT 
+
+echo "Fin"
+exit 0
