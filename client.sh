1467a765981bc7d579ccfb691750d5d7dc31dffb
Hector Puch <hector.puch@enti.cat>
2025-01-20 20:18:15 +0100

    Update

--- client.sh
+++ client.sh
@@ -1,41 +1,89 @@
 #!/bin/bash
 
-echo "LSTP Client(Lechuga Speaker Transfer Protocol)"
+if [ $# -ne 1 ]
+then
+	echo "Error: El comando requiere al menos un parámetro"
+	echo "Ejemplo de uso:"
+	echo -e "\t$0 127.0.0.1"
+	
+	exit 100
+fi
 
-echo "1. SEND HEADER"
+PORT=7777
 
-echo "LSTP_1" | nc localhost 7777
+IP_SERVER=$1
 
-echo "2.LISTEN OK_HEADER"
+IP_CLIENT=`ip a | grep -i inet | grep -i global | awk '{print $2}' | cut -d "/" -f 1`
 
-DATA=`nc -l 7777`
+echo "LSTP Client (Lechuga Speaker Transfer Protocol)"
+
+echo "1. SEND HEADER (Client: $IP_CLIENT, Server: $IP_SERVER)"
+
+echo "LSTP_1 $IP_CLIENT" | nc $IP_SERVER $PORT
+
+echo "2. LISTEN OK_HEADER"
+
+DATA=`nc -l $PORT`
 
 echo "6. CHECK OK_HEADER"
 
 if [ "$DATA" != "OK_HEADER" ]
 then
-	echo "ERROR 1: Header enviado incorrectamente."
+	echo "ERROR 1: Header enviado incorrectamente"
 
 	exit 1
 fi
 
-#cat client/lechuga1.lechu | text2wave -o client/lechuga1.wav
+
+#cat clent/lechuga1.lechu | text2wave -o client/lechuga1.wav
 
 #yes | ffmpeg -i client/lechuga1.wav client/lechuga1.ogg
 
 echo "7. SEND FILE_NAME"
 
-echo "FILE_NAME lechuga.ogg"  | nc localhost 7777
+echo "FILE_NAME lechuga.ogg" | nc $IP_SERVER $PORT
 
-echo "8. LISTEN FILE_NAME"
+echo "8. LISTEN"
 
-FILES=`nc -l 7777`
+DATA=`nc -l $PORT`
 
-echo "10. CHECK FILE_NAME"
-
-if [ "$FILES" != "FILE_NAME" ]
+if [ "$DATA" != "OK_FILE_NAME" ]
 then
-	echo "ERROR 2: FILE_NAME enviado incorrectamente."
+	echo "ERROR 2: FILE_NAME mal enviado"
 
-	exit 2 
+	exit 2
 fi
+
+echo "12. SEND FILE DATA"
+
+cat lechuga.ogg | nc $IP_SERVER $PORT
+
+echo "13. LISTEN OK/KO_FILE_DATA"
+
+DATA=`nc -l $PORT`
+
+if [ "$DATA" != "OK_FILE_DATA" ]
+then
+	echo "ERROR 3: Error al enviar los datos"
+	exit 3
+fi
+
+echo "16. SEND FILE_DATA_MD5"
+
+MD5=`cat lechuga.ogg | md5sum | cut -d " " -f 1`
+
+echo "FILE_DATA_MD5 $MD5" | nc $IP_SERVER $PORT
+
+echo "17. LISTEN OK/KO_FILE_DATA_MD5"
+
+DATA=`nc -l $PORT`
+
+if [ "$DATA" != "OK_FILE_DATA_MD5" ]
+then
+	echo "ERROR 4: FILE_DATA_MD5 mal enviado"
+
+	exit 4
+fi
+
+echo "Fin"
+exit 0
